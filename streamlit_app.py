df4f26b4db9eda0cbbfcba913ea2e6d250a79aa3
NagasakaYUU <117066529+NagasakaYUU@users.noreply.github.com>
2022-10-31 16:38:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.header('Breakfast Menu')
-streamlit.title("My Pearents New Healthy Diner")
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text("🥑🍞Avocado toast")

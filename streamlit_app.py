0c90f3bb87f9e23bb69029f517503d18a60cb462
NagasakaYUU <117066529+NagasakaYUU@users.noreply.github.com>
2022-11-03 13:53:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ streamlit.text("🥑🍞Avocado toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-streamlit.stop()
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -26,6 +25,9 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
+
+streamlit.stop()
+
 
 
 
